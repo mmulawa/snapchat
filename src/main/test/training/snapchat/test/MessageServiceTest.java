ebda6790bc90be6e38f6ee1e21ca323ca8afa104
Marcin <marcinmulawa92@gmail.com>
2016-03-19 19:36:19 +0100

    message service testclass

---
+++ src/main/test/training/snapchat/test/MessageServiceTest.java
@@ -0,0 +1,13 @@
+package training.snapchat.test;
+
+import org.springframework.test.context.ContextConfiguration;
+import training.snapchat.config.ApplicationContext;
+
+/**
+ * Created by Marcin on 2016-03-19.
+ */
+@ContextConfiguration(classes = {ApplicationContext.class})
+public class MessageServiceTest {
+
+
+}
